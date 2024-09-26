4eb27b5bc7aa9eb97229a2f3c48b0e67f5eda076
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2024-09-26 12:35:07 +0200

    feat: allow signed-in users to add idioms

--- src/hooks/useIdioms.tsx
+++ src/hooks/useIdioms.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export type Entry = {
   id: string;
@@ -43,5 +43,23 @@ export const useIdioms = () => {
       queryKey: ["idiomById", id],
       queryFn: () => fetchIdiomById(id),
     });
-  return { getIdioms, getIdiomById, fetchIdioms, fetchIdiomById };
+
+  const postIdiom = (entry: Entry) => {
+    const response = fetch(`${idiomsUrl}/idioms`, {
+      method: "POST",
+      body: JSON.stringify(entry),
+    });
+    return response;
+  };
+  const createIdiomMutation = useMutation({
+    mutationFn: postIdiom,
+  });
+  return {
+    getIdioms,
+    getIdiomById,
+    fetchIdioms,
+    fetchIdiomById,
+    postIdiom,
+    createIdiomMutation,
+  };
 };
